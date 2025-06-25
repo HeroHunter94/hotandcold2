47aad0934070a3c3d073c8a4fd6d6dd96f901043
HeroHunter94 <danilorusso.info94@gmail.com>
2025-06-25 22:27:31 +0200

    Add files via upload

---
+++ array.js
@@ -0,0 +1,21 @@
+
+const array = [
+    {   text :"Frío, frío: tu número es más grande que el mío",
+        img : 'frio1.jpg'
+    },
+    {   text: "Frío, frío: tu número es más pequeño que el mío",
+        img: 'frio2.jpg'
+    },
+    {   text: "Tibio, tibio: tu número es más grande que el mío",
+        img: 'tibia.jpg'
+    },
+    {   text: "Tibio, tibio: tu número es más pequeño que el mío",
+        img: 'tibia2.jpg'
+    },
+    {   text: "Caliente, caliente: tu número es más grande que el mío",
+        img: 'caliente1.jpg'
+    },
+    {   text : "Caliente, caliente: tu número es más pequeño que el mío",
+        img: 'caliente2.jpg'
+    }
+]
